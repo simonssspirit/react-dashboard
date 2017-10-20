dd17352570fa5ed63d5c359299dce8535c4d5c2a
Genady <genady.sergeev@progress.com>
2017-10-20 16:07:15 +0300

    feat: added redux scaffold

---
+++ src/reducers/perios.js
@@ -0,0 +1,14 @@
+var initialState = {
+    period: 3
+};
+
+export const period = (state = initialState, action) => {
+    if (action.type =='PERIOD_CHANGED') {
+        let period = action.payload;
+        let newState = Object.assign({}, state);
+        newState.period = period;
+        return newState;
+    } else {
+        return state;
+    }
+};
