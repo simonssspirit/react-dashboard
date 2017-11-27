f45425583e21ca5cb3b556854d0b15a6e7c88814
Genady <genady.sergeev@progress.com>
2017-11-27 18:00:49 +0200

    feat: added hoc for templates in issues grid

--- src/components/Issues/Issues.js
+++ src/components/Issues/Issues.js
@@ -19,9 +19,9 @@ class Issues extends Component {
                     </div>
                     <div className="col-sm text-sm-right">
                         <ButtonGroup>
-                            <Button value={3} onClick={this.changePeriod}>3 Months</Button>
-                            <Button value={6} onClick={this.changePeriod}>6 Months</Button>
-                            <Button value={12} onClick={this.changePeriod}>12 Months</Button>
+                            <Button value="3" onClick={this.changePeriod}>3 Months</Button>
+                            <Button value="6" onClick={this.changePeriod}>6 Months</Button>
+                            <Button value="12" onClick={this.changePeriod}>12 Months</Button>
                         </ButtonGroup>
                     </div>
                 </div>
--- src/components/Issues/IssuesGrid.js
+++ src/components/Issues/IssuesGrid.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-import { Grid, Column } from '@progress/kendo-react-grid';
+import { Grid, Column, GridCell } from '@progress/kendo-react-grid';
+
+const cellWithTemplate = (Componennt) => {
+    return class extends React.Component {
+        render() {
+            return (<GridCell {...this.props}><Componennt /></GridCell>);
+        }
+    }
+}
+
+const NumberCellTemplate = (props) => {
+    return <a href={props.dataItem.html_url}>{props.dataItem.number}</a>
+}
+
+const TitleCellTemplate = (props) => {
+    let classes = [props.state === 'open' ? 'issue-open': 'issue-closed', 'issue-status'].join(' ');
+    return <span className={classes}>{props.title}</span>
+}
 
 const IssuesGrid = (props) => {
+    const IdTemplate = cellWithTemplate(NumberCellTemplate);
+    const TitleTemplate = cellWithTemplate(TitleCellTemplate);
+
     return(
         <Grid data={props.issues}>
-
+            <Column field="number" cell={IdTemplate} />
+            <Column field="title" cell={TitleTemplate} />
         </Grid>
     );
 }
