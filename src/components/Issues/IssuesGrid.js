56f4ada16a5266678485b13536c8fa49d06de3c5
Genady <genady.sergeev@progress.com>
2017-12-18 18:59:45 +0200

    feat: added row detail template

--- src/components/Issues/IssuesGrid.js
+++ src/components/Issues/IssuesGrid.js
@@ -3,6 +3,8 @@ import { Grid, Column } from '@progress/kendo-react-grid';
 
 import { LabelCellTemplate, TitleCellTemplate, NumberCellTemplate, MilestoneCellTemplate, AssigneeCellTemplate } from './IssuesGridCellTemplates.js';
 
+import Markdown from './Markdown.js';
+
 class IssuesGrid extends React.Component {
     render() {
         return(
@@ -18,7 +20,58 @@ class IssuesGrid extends React.Component {
 }
 
 const RowDetailComponent = (props) => {
-    return <h1>FOO</h1>
+    let dataItem = props.dataItem;
+    let badgeClass = dataItem.state == 'open' ? 'badge-success' : 'badge-danger';
+    return (
+        <div>
+            <div className="row my-4">
+                <div className="col-sm-12">
+                    <span className="badge" className={badgeClass}>{dataItem.state }</span>
+                    <h3 className="h1">
+                        { dataItem.title }
+                        <span className="text-muted">#{ dataItem.number }</span>
+                    </h3>
+                </div>
+            </div>
+            <div className="row my-4">
+                <div className="col-sm-2">
+                    <span className="small d-block text-muted">Created on</span>
+                    { dataItem.created_at }
+                </div>
+                {dataItem.closed_at !== null ?
+                    (<div className="col-sm-2">
+                        <span className="small d-block text-muted">Closed on</span>
+                        { dataItem.closed_at }
+                    </div>)
+                : null}
+                <div className="col-sm-2">
+                    <span className="small d-block text-muted">Milestone</span>
+                    { dataItem.milestone ? dataItem.milestone.title : '' }
+                </div>
+                <div className="col-sm-2">
+                    <span className="small d-block text-muted">Author</span>
+                    { dataItem.user.login }
+                </div>
+                { dataItem.assignee !== null ?
+                    (<div className="col-sm-2">
+                        <span className="small d-block text-muted">Assignee</span>
+                        <img src={dataItem.assignee? dataItem.assignee.avatar_url : undefined} style={{'width': '30px', 'height': '30px'}} className="img-circle" />
+                        { dataItem.assignee ? dataItem.assignee.login : '' }
+                    </div>)
+                : null}
+            </div>
+            <div className="row my-4">
+                <div className="col-sm-2">
+                    <h4 className="small text-muted">Labels</h4>
+                    <LabelCellTemplate dataItem={dataItem} />
+                </div>
+                <div className="col-sm-8">
+                    <h4 className="small text-muted">Description</h4>
+                    <Markdown input={dataItem.body} />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 const cellWithTemplate = (Component) => {
