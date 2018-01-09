c24ede7dfc21de776078274c76a01d26769643b2
Genady <genady.sergeev@progress.com>
2018-01-09 13:48:43 +0200

    feat: profile page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -13,32 +13,105 @@ class Profile extends Component {
         }
     }
 
+    onUpdateClick() {
+    }
+
+    onDeleteClick() {
+    }
+
     render() {
         return (
             this.state &&
         <div>
-
-        <div>
-            <div className="row mb-4">
-                <div className="col-sm">
-                    <h2>
-                        <span className="small text-uppercase text-muted d-block">Account</span>
-                        {this.state.login} - {this.state.name}
-                    </h2>
-                </div>
-                <div className="col-sm text-sm-right">
-                    <button className="btn btn-secondary">Sign out</button>
+        {/* if userId */}
+            <div>
+                <div className="row mb-4">
+                    <div className="col-sm">
+                        <h2>
+                            <span className="small text-uppercase text-muted d-block">Account</span>
+                            {this.state.login} - {this.state.name}
+                        </h2>
+                    </div>
+                    <div className="col-sm text-sm-right">
+                        <button className="btn btn-secondary">Sign out</button>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div className="row">
-            <div className="col-md-7">
+            <div className="row">
+                <div className="col-md-7">
 
+                    <div className="card" id="profile">
+                        <h3 className="card-header">Public Profile</h3>
+                        <div className="card-block">
 
+                            <div className="row">
+                                <div className="col-md-3 text-xs-center">
+                                    <img src={this.state.avatar_url} style={{'max-width': '100%'}} className="img-circle mx-auto" />
+                                </div>
+                                <div className="col-md-9">
+                                    <div className="form-group">
+                                        <label className="h6">Username</label>
+                                        <input type="text" id="username" className="form-control" value={this.state.login} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="h6">Name</label>
+                                        <input type="text" id="name" className="form-control" value={this.state.name} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="h6">Email</label>
+                                        <input type="email" id="email" className="form-control" value={this.state.email} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="form-check-label h6">
+                                            <input type="checkbox" className="form-check-input" checked /> Keep my email address private
+                                        </label>
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="h6">Company</label>
+                                        <input type="text" id="company" className="form-control" value={this.state.company} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="h6">Location</label>
+                                        <input type="text" id="location" className="form-control" value={this.state.location} />
+                                    </div>
+                                    <div className="form-group">
+                                        <button className="btn btn-primary" click={this.onUpdateClick}>Update profile</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="card" id="delete-account">
+                        <h3 className="card-header">Delete Account</h3>
+                        <div className="card-block">
+                            <p>You will immediately lose access to your repositories and all other information associated with your account.
+                                <strong>This cannot be undone!</strong>
+                            </p>
+                            <button className="btn btn-danger" click={this.onDeleteClick}>Delete Account</button>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="col-md-5">
+                    <div className="card" id="notifications">
+                        <h3 className="card-header">Notifications</h3>
+                        <div className="card-block">
+                            <h4 className="h6">Automatically watch repositories?</h4>
+                            <p className="text-muted">When you are given push access to a repository, automatically receive notifications for it.</p>
+                            {/* <p><kendo-switch></kendo-switch></p>
+                            <h4 className="h6">Receive updates to any conversations via email?</h4>
+                            <p><kendo-switch [checked]="true"></kendo-switch></p>
+                            <h4 className="h6">Receive updates to any repositories via email?</h4>
+                            <p><kendo-switch [checked]="true"></kendo-switch></p> */}
+                        </div>
+                    </div>
+                </div>
 
             </div>
-        </div>
 
         </div>
         );
