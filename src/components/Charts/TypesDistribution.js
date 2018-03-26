c45727b95ee56ac59e2ac4b75f54049df5842924
Kiril Peyanski <peyanski@progress.com>
2018-03-26 11:45:26 +0300

    fix: dashboard Types Distribution chart update on new props

--- src/components/Charts/TypesDistribution.js
+++ src/components/Charts/TypesDistribution.js
@@ -2,44 +2,49 @@ import React, { Component } from 'react';
 import { Chart } from '@progress/kendo-charts-react-wrapper';
 
 class TypesDistribution extends Component {
+    seriesColors = [
+        { label: "SEV: Low", value: "#FF9966", active: true },
+        { label: "SEV: Medium", value: "#BB6ACB", active: false },
+        { label: "SEV: High", value: "#52C3D3", active: false },
+        { label: "Enhancement", value: "#22C85D", active: true },
+        { label: "Feature", value: "#FF6358", active: false },
+        { label: "Others", value: "#2BA7DA", active: true }
+    ];
     constructor(props) {
         super(props);
         this.addSeries = this.addSeries.bind(this);
-        let seriesColors = [
-            { label: "SEV: Low", value: "#FF9966", active: true },
-            { label: "SEV: Medium", value: "#BB6ACB", active: false },
-            { label: "SEV: High", value: "#52C3D3", active: false },
-            { label: "Enhancement", value: "#22C85D", active: true },
-            { label: "Feature", value: "#FF6358", active: false },
-            { label: "Others", value: "#2BA7DA", active: true }
-        ];
-        let mapSeries = ((series) => {
-            return {
-                color: series.value,
-                markers: { visible: false },
-                data: this.props.data[series.label]
-            };
-        })
 
         this.state = {
             initialGrey: '#A2ACAC',
-            visibleSeries: seriesColors.filter(s => s.active).map(mapSeries),
-            seriesColors: seriesColors,
-            mapSeries: mapSeries
+            visibleSeries: this.seriesColors.filter(s => s.active).map(this.mapSeries),
+            seriesColors: this.seriesColors,
+            mapSeries: this.mapSeries
         };
     }
+    componentWillReceiveProps(props) {
+        this.setState({
+            visibleSeries: this.seriesColors.filter(s => s.active).map(this.mapSeries),
+        })
+    }
 
-    addSeries(button)
-    {
-        let seriesColors = this.state.seriesColors.map(b => { if (button.value === b.value) { b.active = !b.active } return b;});
+    mapSeries = ((series) => {
+        return {
+            color: series.value,
+            markers: { visible: false },
+            data: this.props.data[series.label]
+        };
+    })
+
+    addSeries(button) {
+        let seriesColors = this.state.seriesColors.map(b => { if (button.value === b.value) { b.active = !b.active } return b; });
         let visibleSeries = seriesColors.filter(s => s.active).map(this.state.mapSeries)
 
-        this.setState({seriesColors, visibleSeries});
+        this.setState({ seriesColors, visibleSeries });
     }
 
     render() {
         const seriesDefaults = { type: 'line', overlay: false };
-        const categoryAxis = [{baseUnit: 'months', majorTicks: { visible: false }, labels: { step: 4, skip: 2}, majorGridLines: { visible: false }, line: { visible: false} }];
+        const categoryAxis = [{ baseUnit: 'months', majorTicks: { visible: false }, labels: { step: 4, skip: 2 }, majorGridLines: { visible: false }, line: { visible: false } }];
         const series = this.state.visibleSeries.map(series => {
             return {
                 data: series.data,
@@ -50,7 +55,7 @@ class TypesDistribution extends Component {
                 categoryField: 'date'
             }
         });
-        const valueAxis = [{ line: {visible: false}, labels: {step: 2, skip: 2}, majorGridLines: {step: 2, skip: 2, color: '#F0F2F2'} }];
+        const valueAxis = [{ line: { visible: false }, labels: { step: 2, skip: 2 }, majorGridLines: { step: 2, skip: 2, color: '#F0F2F2' } }];
 
         return (
             <div className="card">
@@ -61,7 +66,7 @@ class TypesDistribution extends Component {
                             <a
                                 onClick={() => { this.addSeries(button) }}
                                 key={button.label}
-                                style={{color: button.active ? button.value : this.state.initialGrey}}
+                                style={{ color: button.active ? button.value : this.state.initialGrey }}
                                 className="col-6 col-lg-4 col-xl-2 pb-3 comp-label">
                                 <strong>{this.props.data[button.label].length}</strong>
                                 <small>{button.label}</small>
@@ -71,7 +76,7 @@ class TypesDistribution extends Component {
                     }
                 </div>
                 <div className="card-block">
-                    <Chart chartArea={{height: '300px'}}
+                    <Chart chartArea={{ height: '300px' }}
                         transitions={false}
                         seriesDefaults={seriesDefaults}
                         categoryAxis={categoryAxis}
